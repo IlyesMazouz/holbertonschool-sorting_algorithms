15b75a36a45165c409154403fa0726b3bcc8e737
IlyesMazouz <ilyesmazouzz@gmail.com>
2023-05-26 15:01:04 +0200

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,13 +5,14 @@
  * insertion_sort_list - the Insertion sort
  * @list: Double pointer to the head of the list
  */
+
 void insertion_sort_list(listint_t **list)
 {
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	listint_t *curr = (*list)->next;
-
+	listint_t *curr;
+	curr = (*list)->next;
 	while (curr != NULL)
 	{
 		listint_t *key = curr;
